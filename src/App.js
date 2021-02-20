a829cef015f5e02d0c98a73dde68f2db4d445272
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-02-20 21:46:24 +0530

    Added sidebar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import RecommendedVideos from './components/RecommendedVideos';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app__page">
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import '../Sidebar.css';
+import SidebarRow from './SidebarRow';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">     
+            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={WhatshotIcon} title="Trending" />
+            <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />       
+        </div>
+    )
+}
+
+export default Sidebar
