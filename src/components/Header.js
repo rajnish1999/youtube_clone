6562376b0e8a8ae6f4556ef23c87962fe6cdcac9
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-02-20 20:12:11 +0530

    Added styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,31 @@ import SearchIcon from "@material-ui/icons/Search";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import Avatar from "@material-ui/core/Avatar";
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import '../Header.css';
 
 function Header() {
     
     return (
         <div className="header">
-            <h1>i am header</h1>
-            <MenuIcon />
-            <img className="header__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/9/98/YouTube_Logo.svg"
-                alt=""
-            />
-            <input type="text" />
-            <SearchIcon />
-            <VideoCallIcon />
-            <AppsIcon />
-            <Avatar >OP</Avatar>
+            <div className="header__left">
+                <MenuIcon />
+                <img className="header__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/9/98/YouTube_Logo.svg"
+                    alt=""
+                />
+            </div>
+            <div className="header__input">
+                <input type="text" placeholder="Search" />
+                <SearchIcon className="header__inputButton" />
+            </div>
+            <div className="header__icons">
+                <VideoCallIcon classNamer="header_icon"/>
+                <AppsIcon classNamer="header_icon"/>
+                <NotificationsIcon classNamer="header_icon"/>
+                <Avatar >OP</Avatar>
+            </div>
+            
         </div>
     )
 }
