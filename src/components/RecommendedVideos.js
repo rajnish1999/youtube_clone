481f8b757bf4e1262ce592ebfe11d6804852da67
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-02-22 03:03:57 +0530

    Added RecommendedVideos compon and respective styling to it

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -4,7 +4,7 @@ import '../RecommendedVideos.css';
 function RecommendedVideos() {
     return (
         <div className="recommendedVideos">
-            <h1>This is recommended videos</h1>
+            <h2>Recommended</h2>
         </div>
     )
 }
