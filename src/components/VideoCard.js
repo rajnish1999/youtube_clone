8c5ebf5e84d3351fa82405aac495c5c3d0f71805
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-02-22 04:26:43 +0530

    Added styling to VideoCard component

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Avatar from '@material-ui/core/Avatar';
+import  '../VideoCard.css';
 
 function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
     return (
         <div className='videoCard'>
-            <img src={image} alt="" />
-            <div className="videoCard__info">
+            <img className='videoCard__thumbnail' src={image} />
+            <div className='videoCard__info'>
                 <Avatar 
-                    className="videoCard__avatar"
+                    className='videoCard__avatar'
                     alt={channel}
                     src={channelImage}
                 />
-                <div className="videoCard__text">
+                <div className='videoCard__text'>
                     <h4>{title}</h4>
                     <p>{channel}</p>
                     <p>
