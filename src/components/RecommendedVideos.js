24f18b5f1c76043c7ce68b1a83217431d4fc159e
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-02-22 03:50:05 +0530

    Added videoCard component

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -1,10 +1,79 @@
 import React from 'react'
 import '../RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 function RecommendedVideos() {
     return (
         <div className="recommendedVideos">
             <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                <VideoCard 
+                    title="Become a Web Developer in 10 minutes | 2019/20"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage="https://picsum.photos/200/300"
+                    channel="Rajnish"
+                    image="https://picsum.photos/200/300"
+                />
+                
+            </div>
+            
         </div>
     )
 }
